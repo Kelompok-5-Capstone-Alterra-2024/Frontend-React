26c2cfdbd311e9cca63d625948ddf3e3aa2ce922
Hilmi Yahya <hilmiyahya199@gmail.com>
2024-06-01 10:44:01 +0700

    update dashboard

--- frontend-react/src/pages/admin/Transaksi/Transaksi.jsx
+++ frontend-react/src/pages/admin/Transaksi/Transaksi.jsx
@@ -1,6 +1,8 @@
 function Transaksi() {
   return (
-    <div>Transaksi</div>
+    <div className="bg-neutral-50 min-h-screen">
+      <h1 className="text-center text-gray-600 text-xl p-5">Transaksi</h1>
+    </div>
   )
 }
 
