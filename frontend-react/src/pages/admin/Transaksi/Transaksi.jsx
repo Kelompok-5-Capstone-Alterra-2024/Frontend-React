7b404c7421b652d9ef73eb6b32a6e0ab42a6c0ef
Hilmi Yahya <hilmiyahya199@gmail.com>
2024-06-01 14:32:04 +0700

    update transaksi

--- frontend-react/src/pages/admin/Transaksi/Transaksi.jsx
+++ frontend-react/src/pages/admin/Transaksi/Transaksi.jsx
@@ -1,6 +1,13 @@
+import unduhDetail from "../../../assets/images/unduh-detail.png";
+import RiwayatTransaksi from "../../../components/admin/RiwayatTransaksi/RiwayatTransaksi";
+import hand from "../../../assets/images/HandHeart.png"
+import money from "../../../assets/images/Money_fill.png"
+import wallet from "../../../assets/images/Wallet.png"
+
 function Transaksi() {
   return (
     <div className="bg-neutral-50 min-h-screen p-4">
+      {/* card */}
       <div className="w-full max-w-6xl mx-auto grid grid-cols-1 lg:grid-cols-3 gap-5">
         {[
           {
@@ -9,6 +16,7 @@ function Transaksi() {
             textColor: 'text-sky-500',
             label: 'Total Donasi Terkumpul',
             value: 'RP. 3.900.000,00',
+            icon: hand,
           },
           {
             bgColor: 'bg-sky-200',
@@ -16,7 +24,7 @@ function Transaksi() {
             textColor: 'text-cyan-600',
             label: 'Jumlah Transaksi',
             value: '1.300',
-            icon: 'https://via.placeholder.com/40x28',
+            icon: money,
           },
           {
             bgColor: 'bg-green-200',
@@ -24,6 +32,7 @@ function Transaksi() {
             textColor: 'text-green-400',
             label: 'Penghasilan',
             value: 'RP. 1.900.000,00',
+            icon: wallet,
           },
         ].map((item, index) => (
           <div
@@ -34,7 +43,7 @@ function Transaksi() {
           >
             <div className={`${item.bgColor} p-2.5 rounded-xl flex justify-center items-center`}>
               {item.icon ? (
-                <img className="w-10 h-7" src={item.icon} alt="icon" />
+                <img className="w-10 h-10" src={item.icon} alt="icon" />
               ) : (
                 <div className="w-12 h-12" />
               )}
@@ -45,6 +54,32 @@ function Transaksi() {
             </div>
           </div>
         ))}
+      </div>
+      {/* tabs */}
+      <div className="flex flex-wrap justify-between">
+        {/* Bagian kiri */}
+        <div className="left">
+          <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200">
+          <ul className="flex">
+          <li>
+            <a href="" className="inline-block pt-5 pb-2 px-10 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300">Riwayat Donasi</a>
+          </li>
+          <li>
+            <a href="" className="inline-block pt-5 pb-2 px-10 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active" aria-current="page">Riwayat Transaksi</a>
+          </li>
+          </ul>
+          </div>
+        </div>
+        {/* Bagian kanan */}
+        <div className="right p-4">
+          <button>
+            <img className="w-28 h-10" src={unduhDetail} alt="Unduh Detail" />
+          </button>
+        </div>
+      </div>
+      {/* Table */}
+      <div>
+        <RiwayatTransaksi/>
       </div>
     </div>
   );
