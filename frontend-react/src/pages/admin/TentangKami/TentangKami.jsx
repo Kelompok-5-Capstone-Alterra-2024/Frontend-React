c8cfc691660fbc596ebcec94b3fab692b4d52c1f
Hilmi Yahya <hilmiyahya199@gmail.com>
2024-06-02 08:26:32 +0700

    delete tentang kami

--- frontend-react/src/pages/admin/TentangKami/TentangKami.jsx
+++
@@ -1,9 +0,0 @@
-function TentangKami() {
-  return (
-    <div className="bg-neutral-50 min-h-screen">
-      <h1 className="text-center text-gray-600 text-xl p-5">Tentang Kami</h1>
-    </div>
-  )
-}
-
-export default TentangKami
