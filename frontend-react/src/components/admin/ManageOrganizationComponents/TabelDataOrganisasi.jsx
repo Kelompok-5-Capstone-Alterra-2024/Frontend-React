851c844ef389bab56e0ded2700727cf1ca007740
Hilmi Yahya <hilmiyahya199@gmail.com>
2024-06-03 06:44:01 +0700

    update manage-organisasi

---
+++ frontend-react/src/components/admin/ManageOrganizationComponents/TabelDataOrganisasi.jsx
@@ -0,0 +1,77 @@
+import edit from '../../../assets/images/PencilSimple.svg';
+import trash from '../../../assets/images/Trash.svg'
+
+const TabelDataOrganisasi = () => {
+    const data = [
+      {
+        no: 1,
+        namaOrganisasi: "Kalasahan Maratua Indonesia",
+        tanggalBergabung: "29 Mei 2024",
+        kontak: "@sosialmediaclimate",
+        status: "selesai",
+      },
+      {
+        no: 2,
+        namaOrganisasi: "Kalasahan Maratua Indonesia",
+        tanggalBergabung: "29 Mei 2024",
+        kontak: "@sosialmediaclimate",
+        status: "selesai",
+      },
+      {
+        no: 3,
+        namaOrganisasi: "Kalasahan Maratua Indonesia",
+        tanggalBergabung: "29 Mei 2024",
+        kontak: "@sosialmediaclimate",
+        status: "selesai",
+      }
+    ];
+  
+    return (
+      <div className="overflow-x-auto shadow-md mt-5">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+          <thead className="text-xs text-white uppercase bg-primary-main">
+            <tr>
+              <th scope="col" className="px-6 py-3">No</th>
+              <th scope="col" className="px-6 py-3">
+                <div className="flex items-center">
+                  Nama Organisasi
+                  <a href="#">
+                    <svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                      <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                    </svg>
+                  </a>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">Tanggal Bergabung</th>
+              <th scope="col" className="px-6 py-3">Kontak</th>
+              <th scope="col" className="px-6 py-3">Status</th>
+              <th scope="col" className="px-6 py-3">Aksi</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {item.no}
+                </th>
+                <td className="px-6 py-4">{item.namaOrganisasi}</td>
+                <td className="px-6 py-4">{item.tanggalBergabung}</td>
+                <td className="px-6 py-4">{item.kontak}</td>
+                <td className="px-6 py-4">
+                    <div className="px-2 py-1 bg-emerald-100 rounded justify-center items-center gap-2.5 inline-flex">
+                        <div className="text-neutral-700 text-xs font-normal font-['Roboto'] leading-[18px]">{item.status}</div>
+                    </div>
+                </td>
+                <td className="px-6 py-4 flex">
+                    <img className="pe-3" src={edit} alt="edit" />
+                    <img src={trash} alt="hapus" />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+  
+  export default TabelDataOrganisasi;
