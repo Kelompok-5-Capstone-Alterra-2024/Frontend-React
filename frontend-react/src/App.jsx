60720ae73e450aed1e14aad9d18ae3fa485d2904
Hilmi Yahya <hilmiyahya199@gmail.com>
2024-06-03 20:26:43 +0700

    Update App.jsx

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -8,13 +8,13 @@ import NotFound from "./pages/NotFound/NotFound"
 import Transaksi from "./pages/admin/Transaksi/Transaksi"
 import ManageOrganization from "./pages/admin/ManageOrganization/ManageOrganization"
 import ManageUser from "./pages/admin/ManageUser/ManageUser"
+import DetailUser from "./components/admin/ManageUserComponents/DetailUser"
 import Donasi from "./pages/admin/ManageContent/Donasi/Donasi"
 import DetailDonasi from "./components/admin/ManageContentDonasi/DetailDonasi"
 import Relawan from "./pages/admin/ManageContent/Relawan/Relawan"
 import Artikel from "./pages/admin/ManageContent/Artikel/Artikel"
 import Chatbot from "./pages/admin/Chatbot/Chatbot"
 import Notification from "./pages/admin/Notification/Notification"
-
 
 function App() {
 
@@ -32,6 +32,7 @@ function App() {
           <Route path="manage-content/relawan" element={<Relawan />} />
           <Route path="manage-content/artikel" element={<Artikel />} />
           <Route path="manage-user" element={<ManageUser />} />
+          <Route path="manage-user/detail-user" element={<DetailUser />} />
           <Route path="manage-organization" element={<ManageOrganization />} />
           <Route path="transaksi" element={<Transaksi />} />
           <Route path="chatbot" element={<Chatbot />} />
