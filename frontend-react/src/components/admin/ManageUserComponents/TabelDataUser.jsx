fb0480a69b4874c4a5d6ffd453e7b5d49b7c6bb9
Hilmi Yahya <hilmiyahya199@gmail.com>
2024-06-03 22:19:17 +0700

    update manage-user

--- frontend-react/src/components/admin/ManageUserComponents/TabelDataUser.jsx
+++ frontend-react/src/components/admin/ManageUserComponents/TabelDataUser.jsx
@@ -1,7 +1,89 @@
-function TabelDataUser() {
-  return (
-    <div>TabelDataUser</div>
-  )
-}
+import { Link } from "react-router-dom";
+import lihat from "../../../assets/images/ButtonLihat.png"
 
-export default TabelDataUser
+const TableDataUser = () => {
+    const data = [
+        {
+            id: 1,
+            fullName: "Agung Rizky",
+            email: "agung123@gmail.com",
+            nomorTelepon: "081290575320",
+            tanggalRegister: "12 November 2024",
+            aksi: "Lihat",
+        },
+        {
+            id: 2,
+            fullName: "Agung Rizky",
+            email: "agung123@gmail.com",
+            nomorTelepon: "081290575320",
+            tanggalRegister: "12 November 2024",
+            aksi: "Lihat",
+        },
+        {
+            id: 3,
+            fullName: "Agung Rizky",
+            email: "agung123@gmail.com",
+            nomorTelepon: "081290575320",
+            tanggalRegister: "12 November 2024",
+            aksi: "Lihat",
+        }
+    ];
+  
+    return (
+      <div className="relative overflow-x-auto shadow-md mt-5">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+          <thead className="text-xs text-white uppercase bg-primary-main">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                <div className="flex items-center">
+                  ID
+                  <a href="#">
+                    <svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                      <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                    </svg>
+                  </a>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">Full Name</th>
+              <th scope="col" className="px-6 py-3">Email</th>
+              <th scope="col" className="px-6 py-3">Nomor Telepon</th>
+              <th scope="col" className="px-6 py-3">
+                <div className="flex items-center">
+                  Tanggal Register
+                  <a href="#">
+                    <svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                      <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                    </svg>
+                  </a>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">Aksi</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {item.id}
+                </th>
+                <td className="px-6 py-4">{item.fullName}</td>
+                <td className="px-6 py-4">{item.email}</td>
+                <td className="px-6 py-4">{item.nomorTelepon}</td>
+                <td className="px-6 py-4">{item.tanggalRegister}</td>
+                <td className="px-6 py-1">
+                    <Link to="detail-user">
+                    <button className="relative h-12 w-12">
+                        <img src={lihat} alt="Lihat" />
+                    </button>
+                    </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
+  
+  export default TableDataUser;
+  
--- frontend-react/src/pages/admin/ManageUser/ManageUser.jsx
+++ frontend-react/src/pages/admin/ManageUser/ManageUser.jsx
@@ -1,9 +1,35 @@
-function ManageUser() {
-  return (
-    <div className="bg-neutral-50 min-h-screen">
-      <h1 className="text-center text-gray-600 text-xl p-5">Manage User</h1>
-    </div>
-  )
-}
+import PaginationDataUser from "../../../components/admin/ManageUserComponents/PaginationDataUser";
+import TabelDataUser from "../../../components/admin/ManageUserComponents/TabelDataUser";
 
-export default ManageUser
+const ManageUser = () => {
+    return (
+      <div className="bg-neutral-50 min-h-screen p-4">
+      <div className="p-6 shadow-lg rounded-lg">
+        <div className="flex flex-wrap justify-between items-center">
+          <div>
+            <h1 className="text-2xl font-bold text-black">Manage User</h1>
+            <p className="text-gray-500 pt-2">Berikut list user PeduliPintar</p>
+          </div>
+          <div className="mt-4">
+            <label className="input input-bordered flex items-center gap-2 bg-white border border-gray-300 rounded-lg shadow-sm focus-within:ring-2 focus-within:ring-indigo-500">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70">
+                <path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" />
+              </svg>
+              <input
+                type="text"
+                className="grow border-0 focus:outline-none focus:ring-0 lg:w-96"
+                placeholder="Search"
+              />
+            </label>
+          </div>
+        </div>
+        <TabelDataUser/>
+        <PaginationDataUser/>
+      </div>
+      </div>
+    );
+  };
+  
+  export default ManageUser;
+  
+
