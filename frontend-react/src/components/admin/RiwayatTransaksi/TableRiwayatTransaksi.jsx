31fbe0416d4ce4192d6dd25b3a2e4cb528a91371
Hilmi Yahya <hilmiyahya199@gmail.com>
2024-06-07 20:59:37 +0700

    update

--- frontend-react/src/components/admin/RiwayatTransaksi/TableRiwayatTransaksi.jsx
+++ frontend-react/src/components/admin/RiwayatTransaksi/TableRiwayatTransaksi.jsx
@@ -7,7 +7,7 @@ const TableRiwayatTransaksi = () => {
       pembayaran: "Transfer Bank",
       noRekening: "3098717893290",
       tanggal: "27-04-2024",
-      status: "selesai"
+      status: "Detail"
     },
     {
       no: 2,
@@ -16,7 +16,7 @@ const TableRiwayatTransaksi = () => {
       pembayaran: "Transfer Bank",
       noRekening: "3098717893290",
       tanggal: "27-04-2024",
-      status: "selesai"
+      status: "Detail"
     },
     {
       no: 3,
@@ -25,7 +25,7 @@ const TableRiwayatTransaksi = () => {
       pembayaran: "Transfer Bank",
       noRekening: "3098717893290",
       tanggal: "27-04-2024",
-      status: "selesai"
+      status: "Detail"
     }
   ];
 
