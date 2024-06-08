e1302a584e14ed2f49f73cdd9e9773ccab82922b
lativayuliaa <21081010294@student.upnjatim.ac.id>
2024-06-08 19:37:45 +0700

    Update DetailDonasi.jsx

--- frontend-react/src/components/admin/ManageContentDonasi/DetailDonasi/DetailDonasi.jsx
+++ frontend-react/src/components/admin/ManageContentDonasi/DetailDonasi/DetailDonasi.jsx
@@ -95,8 +95,8 @@ function DetailDonasi() {
             <div className="w-full p-2 bg-white rounded-lg shadow flex-col justify-start items-start gap-4 flex">
               <h1 className="text-cyan-600 font-bold">Deskripsi</h1>
               <div className="text-justify">
-                <span className="text-black text-sm font-normal">{data.description}</span>
-                <span className="text-cyan-600 text-sm font-normal">Read More</span>
+                <span className="text-black text-md font-normal">{data.description}</span>
+                <span className="text-cyan-600 text-md font-normal">Read More</span>
               </div>
             </div>
           </div>
@@ -108,117 +108,117 @@ function DetailDonasi() {
           </div>
         </div>
 
-        <div className="bg-white p-4 rounded-lg shadow max-h-screen overflow-y-auto">
-          <form>
-            <div className="mb-6">
-              <div className="flex flex-col sm:flex-row items-center">
-                <label htmlFor="multiple_files" className="text-md w-full sm:w-28 text-white bg-sky-500 border border-gray-300 rounded-t-lg sm:rounded-e-none sm:rounded-l-lg cursor-pointer p-2 text-center">
-                  Pilih File
-                </label>
-                <span className="p-2 text-gray-500 border border-gray-300 w-full rounded-b-lg sm:rounded-b-none sm:rounded-r-lg sm:w-auto sm:mt-0 sm:flex-1" id="file_name">Tidak ada file yang dipilih</span>
+        <div className="bg-white p-4 rounded-lg shadow max-h-screen overflow-y-auto h-full flex flex-col">
+        <form className="flex flex-col flex-1">
+          <div className="mb-6">
+            <div className="flex flex-col sm:flex-row items-center">
+              <label htmlFor="multiple_files" className="text-md w-full sm:w-28 text-white bg-sky-500 border border-gray-300 rounded-t-lg sm:rounded-e-none sm:rounded-l-lg cursor-pointer p-2 text-center">
+                Pilih File
+              </label>
+              <span className="p-2 text-gray-500 border border-gray-300 w-full rounded-b-lg sm:rounded-b-none sm:rounded-r-lg sm:w-auto sm:mt-0 sm:flex-1" id="file_name">Tidak ada file yang dipilih</span>
+              <input
+                className="hidden"
+                id="multiple_files"
+                type="file"
+                multiple
+              />
+            </div>
+          </div>
+          <div className="mb-6 flex-1 flex flex-col">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <div className="flex flex-col">
+                <label htmlFor="judul" className="block text-gray-700 text-sm font-normal">Judul</label>
                 <input
-                  className="hidden"
-                  id="multiple_files"
-                  type="file"
-                  multiple
+                  type="text"
+                  id="judul"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  placeholder="Type here"
+                  className="mt-1 border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="organisasi" className="block text-gray-700 text-sm font-normal">Organisasi</label>
+                <select
+                  id="organisasi"
+                  value={organizationName}
+                  onChange={(e) => setOrganizationName(e.target.value)}
+                  className="mt-1 border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 sm:text-sm bg-gray-100"
+                >
+                  {organizations.map((org, index) => (
+                    <option key={index} value={org}>{org}</option>
+                  ))}
+                </select>
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="target" className="block text-gray-700 text-sm font-normal">Target Donasi</label>
+                <input
+                  type="number"
+                  id="target"
+                  value={targetAmount}
+                  onChange={(e) => setTargetAmount(e.target.value)}
+                  placeholder="Type here"
+                  className="mt-1 border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="kategori" className="block text-gray-700 text-sm font-normal">Kategori</label>
+                <select
+                  id="kategori"
+                  value={category}
+                  onChange={(e) => setCategory(e.target.value)}
+                  className="mt-1 border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 sm:text-sm bg-gray-100"
+                >
+                  {categories.map((cat, index) => (
+                    <option key={index} value={cat}>{cat}</option>
+                  ))}
+                </select>
+              </div>
+              <div className="grid grid-rows-1 md:grid-rows-2 gap-4">
+                <div className="flex flex-col">
+                  <label htmlFor="tanggalMulai" className="block text-gray-700 text-sm font-normal mb-1">Tanggal mulai</label>
+                  <input
+                    type="date"
+                    id="tanggalMulai"
+                    value={startDate}
+                    onChange={(e) => setStartDate(e.target.value)}
+                    className="mt-1 border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label htmlFor="tanggalSelesai" className="block text-gray-700 text-sm font-normal mb-1">Tanggal selesai</label>
+                  <input
+                    type="date"
+                    id="tanggalSelesai"
+                    value={endDate}
+                    onChange={(e) => setEndDate(e.target.value)}
+                    className="mt-1 border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col flex-1">
+                <label htmlFor="deskripsi" className="block text-gray-700 text-sm font-normal mb-1">Deskripsi</label>
+                <textarea
+                  id="deskripsi"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  className="block p-2.5 w-full h-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 flex-1"
                 />
               </div>
             </div>
-            <div className="mb-6">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <div className="flex flex-col">
-                  <label htmlFor="judul" className="block text-gray-700 text-sm font-normal">Judul</label>
-                  <input
-                    type="text"
-                    id="judul"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    placeholder="Type here"
-                    className="mt-1 border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                  />
-                </div>
-                <div className="flex flex-col">
-                  <label htmlFor="organisasi" className="block text-gray-700 text-sm font-normal">Organisasi</label>
-                  <select
-                    id="organisasi"
-                    value={organizationName}
-                    onChange={(e) => setOrganizationName(e.target.value)}
-                    className="mt-1 border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 sm:text-sm bg-gray-100"
-                  >
-                    {organizations.map((org, index) => (
-                      <option key={index} value={org}>{org}</option>
-                    ))}
-                  </select>
-                </div>
-                <div className="flex flex-col">
-                  <label htmlFor="target" className="block text-gray-700 text-sm font-normal">Target Donasi</label>
-                  <input
-                    type="number"
-                    id="target"
-                    value={targetAmount}
-                    onChange={(e) => setTargetAmount(e.target.value)}
-                    placeholder="Type here"
-                    className="mt-1 border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                  />
-                </div>
-                <div className="flex flex-col">
-                  <label htmlFor="kategori" className="block text-gray-700 text-sm font-normal">Kategori</label>
-                  <select
-                    id="kategori"
-                    value={category}
-                    onChange={(e) => setCategory(e.target.value)}
-                    className="mt-1 border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 sm:text-sm bg-gray-100"
-                  >
-                    {categories.map((cat, index) => (
-                      <option key={index} value={cat}>{cat}</option>
-                    ))}
-                  </select>
-                </div>
-                <div className="grid grid-rows-1 md:grid-rows-2 gap-4">
-                  <div className="flex flex-col">
-                    <label htmlFor="tanggalMulai" className="block text-gray-700 text-sm font-normal mb-1">Tanggal mulai</label>
-                    <input
-                      type="date"
-                      id="tanggalMulai"
-                      value={startDate}
-                      onChange={(e) => setStartDate(e.target.value)}
-                      className="mt-1 border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                    />
-                  </div>
-                  <div className="flex flex-col">
-                    <label htmlFor="tanggalSelesai" className="block text-gray-700 text-sm font-normal mb-1">Tanggal selesai</label>
-                    <input
-                      type="date"
-                      id="tanggalSelesai"
-                      value={endDate}
-                      onChange={(e) => setEndDate(e.target.value)}
-                      className="mt-1 border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                    />
-                  </div>
-                </div>
-                <div className="flex flex-col">
-                  <label htmlFor="deskripsi" className="block text-gray-700 text-sm font-normal mb-1">Deskripsi</label>
-                  <textarea
-                    id="deskripsi"
-                    rows={5}
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    className="block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="flex justify-end mt-4">
-              <button className="px-6 py-3 bg-gray-100 text-gray-800 rounded-lg gap-2 me-3" type="button" onClick={openDeleteModal}>
-                Hapus
-              </button>
-              <HapusDetailDonasi isOpen={isDeleteModalOpen} onClose={closeDeleteModal} />
-              <button className="px-6 py-3 bg-sky-500 text-white rounded-lg font-semibold" type="button">
-                Edit Konten
-              </button>
-            </div>
-          </form>
-        </div>
+          </div>
+          <div className="flex justify-end mt-4">
+            <button className="px-6 py-3 bg-gray-100 text-gray-800 rounded-lg gap-2 me-3" type="button" onClick={openDeleteModal}>
+              Hapus
+            </button>
+            <HapusDetailDonasi isOpen={isDeleteModalOpen} onClose={closeDeleteModal} />
+            <button className="px-6 py-3 bg-sky-500 text-white rounded-lg font-semibold" type="button">
+              Edit Konten
+            </button>
+          </div>
+        </form>
+      </div>
+
       </div>
       <RiwayatDataDonasi id={id} />
     </div>
