8dddddc4bcdd9b580a0e9c9edbbf104a47588c7a
Hilmi Yahya <hilmiyahya199@gmail.com>
2024-06-12 21:25:00 +0700

    update

--- frontend-react/src/components/admin/ManageUserComponents/DetailUser/Donasi/DetailUserDonasi.jsx
+++ frontend-react/src/components/admin/ManageUserComponents/DetailUser/Donasi/DetailUserDonasi.jsx
@@ -1,14 +1,145 @@
+import { useEffect, useState } from "react";
 import PaginationDetailUserDonasi from "./PaginationDetailUserDonasi";
-import TabelDetailUserDonasi from "./TabelDetailUserDonasi";
 
-const DetailUserDonasi = () => {
+const DetailUserDonasi = ({id}) => {
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const accessToken = sessionStorage.getItem('access_token');
+
+  useEffect(() => {
+    getUserDonation();
+  }, [currentPage]);
+
+  const getUserDonation = async (page) => {
+    try {
+      const response = await fetch(`https://capstone-alterra-424313.as.r.appspot.com/api/v1/admin/users/${id}/donations?page=${page}&limit=5`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const result = await response.json();
+      if (result.status) {
+        setData(result.data);
+      } else {
+        console.error('Failed to fetch donations:', result.message);
+      }
+    } catch (error) {
+      console.error('Failed to fetch donations:', error);
+    }
+  };
+
+  if (!data || data.length === 0) {
     return (
       <div className="pt-5 rounded-lg">
-        <TabelDetailUserDonasi/>
-        <PaginationDetailUserDonasi/>
+      <div className="relative overflow-x-auto shadow-md">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+          <thead className="text-xs text-white uppercase bg-primary-main">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                <div className="flex items-center">
+                  ID Transaksi
+                  <a href="#">
+                    <svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                      <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                    </svg>
+                  </a>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">Nama Program Donasi</th>
+              <th scope="col" className="px-6 py-3">Nama Organisasi</th>
+              <th scope="col" className="px-6 py-3">
+                <div className="flex items-center">
+                  Nominal Donasi
+                  <a href="#">
+                    <svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                      <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                    </svg>
+                  </a>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">
+                <div className="flex items-center">
+                  Tanggal Transaksi
+                  <a href="#">
+                    <svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                      <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                    </svg>
+                  </a>
+                </div>
+              </th>
+            </tr>
+          </thead>
+        </table>
       </div>
+    </div>
     );
-  };
+  }
   
-  export default DetailUserDonasi;
+  return (
+    <div className="pt-5 rounded-lg">
+      <div className="relative overflow-x-auto shadow-md">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+          <thead className="text-xs text-white uppercase bg-primary-main">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                <div className="flex items-center">
+                  ID Transaksi
+                  <a href="#">
+                    <svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                      <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                    </svg>
+                  </a>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">Nama Program Donasi</th>
+              <th scope="col" className="px-6 py-3">Nama Organisasi</th>
+              <th scope="col" className="px-6 py-3">
+                <div className="flex items-center">
+                  Nominal Donasi
+                  <a href="#">
+                    <svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                      <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                    </svg>
+                  </a>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">
+                <div className="flex items-center">
+                  Tanggal Transaksi
+                  <a href="#">
+                    <svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                      <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                    </svg>
+                  </a>
+                </div>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {item.id}
+                </th>
+                <td className="px-6 py-4">{item.fundraising_name}</td>
+                <td className="px-6 py-4">{item.organization_name}</td>
+                <td className="px-6 py-4">{item.amount}</td>
+                <td className="px-6 py-4">{item.transaction_date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <PaginationDetailUserDonasi currentPage={currentPage} onPageChange={setCurrentPage}/>
+    </div>
+  );
+};
+  
+export default DetailUserDonasi;
   
--- frontend-react/src/components/admin/ManageUserComponents/DetailUser/Volunteer/DetailUserVolunteer.jsx
+++ frontend-react/src/components/admin/ManageUserComponents/DetailUser/Volunteer/DetailUserVolunteer.jsx
@@ -1,13 +1,165 @@
+import { useEffect, useState } from "react";
 import PaginationDetailUserVolunteer from "./PaginationDetailUserVolunteer";
-import TabelDetailUserVolunteer from "./TabelDetailUserVolunteer";
 
-const DetailUserVolunteer = () => {
+const DetailUserVolunteer = ({id}) => {
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const accessToken = sessionStorage.getItem('access_token');
+
+  useEffect(() => {
+    getUserVolunteer();
+  }, [currentPage]);
+
+  const getUserVolunteer = async (page) => {
+    try {
+      const response = await fetch(`https://capstone-alterra-424313.as.r.appspot.com/api/v1/admin/users/${id}/volunteers?page=${page}&limit=5`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const result = await response.json();
+      if (result.status) {
+        setData(result.data);
+      } else {
+        console.error('Failed to fetch donations:', result.message);
+      }
+    } catch (error) {
+      console.error('Failed to fetch donations:', error);
+    }
+  };
+
+  if (!data || data.length === 0) {
     return (
       <div className="pt-5 rounded-lg">
-        <TabelDetailUserVolunteer/>
-        <PaginationDetailUserVolunteer/>
-      </div>
+      <div className="relative overflow-x-auto shadow-md">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+        <thead className="text-xs text-white uppercase bg-primary-main">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              <div className="flex items-center">
+                ID Volunteer
+                <a href="#">
+                  <svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                  </svg>
+                </a>
+              </div>
+            </th>
+            <th scope="col" className="px-6 py-3">Nama Program Volunteer</th>
+            <th scope="col" className="px-6 py-3">Nama Organisasi</th>
+            <th scope="col" className="px-6 py-3">
+              <div className="flex items-center">
+                Tanggal Daftar
+                <a href="#">
+                  <svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                  </svg>
+                </a>
+              </div>
+            </th>
+            <th scope="col" className="px-6 py-3">
+              <div className="flex items-center">
+                Tanggal Mulai
+                <a href="#">
+                  <svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                  </svg>
+                </a>
+              </div>
+            </th>
+            <th scope="col" className="px-6 py-3">
+              <div className="flex items-center">
+                Tanggal Berakhir
+                <a href="#">
+                  <svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                  </svg>
+                </a>
+              </div>
+            </th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+    </div>
     );
-  };
+  }
   
-  export default DetailUserVolunteer;
+  return (
+    <div className="pt-5 rounded-lg">
+      <div className="relative overflow-x-auto shadow-md">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+        <thead className="text-xs text-white uppercase bg-primary-main">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              <div className="flex items-center">
+                ID Volunteer
+                <a href="#">
+                  <svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                  </svg>
+                </a>
+              </div>
+            </th>
+            <th scope="col" className="px-6 py-3">Nama Program Volunteer</th>
+            <th scope="col" className="px-6 py-3">Nama Organisasi</th>
+            <th scope="col" className="px-6 py-3">
+              <div className="flex items-center">
+                Tanggal Daftar
+                <a href="#">
+                  <svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                  </svg>
+                </a>
+              </div>
+            </th>
+            <th scope="col" className="px-6 py-3">
+              <div className="flex items-center">
+                Tanggal Mulai
+                <a href="#">
+                  <svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                  </svg>
+                </a>
+              </div>
+            </th>
+            <th scope="col" className="px-6 py-3">
+              <div className="flex items-center">
+                Tanggal Berakhir
+                <a href="#">
+                  <svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                  </svg>
+                </a>
+              </div>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {item.id}
+              </th>
+              <td className="px-6 py-4">{item.volunteer_name}</td>
+              <td className="px-6 py-4">{item.organization_name}</td>
+              <td className="px-6 py-4">{item.registration_date}</td>
+              <td className="px-6 py-4">{item.start_date}</td>
+              <td className="px-6 py-4">{item.end_date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+    <PaginationDetailUserVolunteer currentPage={currentPage} onPageChange={setCurrentPage}/>
+    </div>
+  );
+};
+  
+export default DetailUserVolunteer;
