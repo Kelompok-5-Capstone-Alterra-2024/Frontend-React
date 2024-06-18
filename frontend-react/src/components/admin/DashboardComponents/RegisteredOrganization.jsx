d67e82f797ab6e4edc8db6928fe5343fe86d847c
lativayuliaa <21081010294@student.upnjatim.ac.id>
2024-06-18 11:00:11 +0700

    update homepage

--- frontend-react/src/components/admin/DashboardComponents/RegisteredOrganization.jsx
+++ frontend-react/src/components/admin/DashboardComponents/RegisteredOrganization.jsx
@@ -1,6 +1,26 @@
+import right from '../../../assets/images/rightIcon.svg';
+
 function RegisteredOrganization() {
   return (
-    <div>RegisteredOrganization</div>
+    <div className="md:col-span-1 bg-white p-4 rounded-lg shadow flex flex-col items-center">
+      <div className="w-full flex flex-col mb-4">
+        <p className="text-zinc-900 text-base pb-1 font-bold">Top Organisasi</p>
+        <p className="text-zinc-500 text-sm">Organisasi yang terdaftar saat ini</p>
+      </div>
+      <div className="flex-1 w-full">
+        <div className="flex items-center p-2">
+          <img className="w-9 h-9 rounded-full" src="https://via.placeholder.com/36x36" alt="Content" />
+          <div className="ml-2">
+            <p className="text-zinc-900 text-sm font-semibold">Kesalahan Maratua Indonesia</p>
+            <p className="text-zinc-500 text-sm">yayasankalasahan@gmail.com</p>
+          </div>
+        </div>
+        <button className="bg-gray-100 flex text-sky-500 text-xs py-1 px-4 rounded border mt-4">
+          Lihat Semua Organisasi
+          <img className="w-3 m-1" src={right} alt="right" />
+        </button>
+      </div>
+    </div>
   )
 }
 
--- frontend-react/src/components/admin/DashboardComponents/TotalAllContent.jsx
+++ frontend-react/src/components/admin/DashboardComponents/TotalAllContent.jsx
@@ -1,6 +1,51 @@
+import up from '../../../assets/images/arrowUp.svg';
+
 function TotalAllContent() {
   return (
-    <div>TotalAllContent</div>
+    <div className="">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="bg-white p-5 rounded-lg border border-gray-200 flex flex-col">
+          <p className="text-zinc-500 text-xs pb-1">DONASI TERKUMPUL</p>
+          <div className="flex justify-between items-center w-full">
+            <p className="text-zinc-900 text-xl font-semibold">Rp.3.900.000,00</p>
+            <div className="flex items-center gap-0">
+              <p className="text-green-500 text-sm">+ 12%</p>
+              <img className="w-2 ml-1" src={up} alt="Arrow Up" />
+            </div>
+          </div>
+        </div>
+        <div className="bg-white p-5 rounded-lg border border-gray-200 flex flex-col">
+          <p className="text-zinc-500 text-xs pb-1">JUMLAH RELAWAN</p>
+          <div className="flex justify-between items-center w-full">
+            <p className="text-zinc-900 text-xl font-semibold">2.900 Orang</p>
+            <div className="flex items-center gap-0">
+              <p className="text-green-500 text-sm">+ 8%</p>
+              <img className="w-2 ml-1" src={up} alt="Arrow Up" />
+            </div>
+          </div>
+        </div>
+        <div className="bg-white p-5 rounded-lg border border-gray-200 flex flex-col">
+          <p className="text-zinc-500 text-xs">JUMLAH ARTIKEL UPLOAD</p>
+          <div className="flex justify-between items-center w-full">
+            <p className="text-zinc-900 text-xl font-semibold">426</p>
+            <div className="flex items-center gap-0">
+              <p className="text-green-500 text-sm">+ 24%</p>
+              <img className="w-2 ml-1" src={up} alt="Arrow Up" />
+            </div>
+          </div>
+        </div>
+        <div className="bg-white p-5 rounded-lg border border-gray-200 flex flex-col">
+          <p className="text-zinc-500 text-xs">JUMLAH TRANSAKSI</p>
+          <div className="flex justify-between items-center w-full">
+            <p className="text-zinc-900 text-xl font-semibold">700</p>
+            <div className="flex items-center gap-0">
+              <p className="text-green-500 text-sm">+ 9%</p>
+              <img className="w-2 ml-1" src={up} alt="Arrow Up" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
