fa4ad3a2b5a0fd3c8b0603b085bf196e49c4db06
lativayuliaa <21081010294@student.upnjatim.ac.id>
2024-06-20 09:18:23 +0700

    update artikel

--- frontend-react/src/components/admin/ManageContentArtikel/TabelDataArtikel.jsx
+++ frontend-react/src/components/admin/ManageContentArtikel/TabelDataArtikel.jsx
@@ -25,7 +25,7 @@ const TabelDataArtikel = ({data}) => {
       <table className="w-full text-sm text-left rtl:text-right text-gray-500">
         <thead className="text-xs text-white uppercase bg-primary-main">
           <tr>
-            <th scope="col" className="px-6 py-3">no</th>
+            <th scope="col" className="px-6 py-3">id</th>
             <th scope="col" className="px-6 py-3">
               <div className="flex items-center">
                 judul
@@ -52,7 +52,7 @@ const TabelDataArtikel = ({data}) => {
       <table className="w-full text-sm text-left rtl:text-right text-gray-500">
         <thead className="text-xs text-white uppercase bg-primary-main">
           <tr>
-            <th scope="col" className="px-6 py-3">no</th>
+            <th scope="col" className="px-6 py-3">id</th>
             <th scope="col" className="px-6 py-3">
               <div className="flex items-center">
                 judul
@@ -73,7 +73,7 @@ const TabelDataArtikel = ({data}) => {
           {data.map((item, index) => (
             <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
               <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                {index + 1}
+                {item.id}
               </th>
               <td className="px-6 py-4">{item.title}</td>
               <td className="px-6 py-4">{item.content}</td>
